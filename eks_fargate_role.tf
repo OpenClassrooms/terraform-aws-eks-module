e8b43da4d0d63ecea13c9f454e4a36f3d267a1bf
jeremygovi <jeremygovi@gmail.com>
2022-04-24 21:14:17 +0200

    add possibility to choose between fargate and managed nodes

--- eks_fargate_role.tf
+++ eks_fargate_role.tf
@@ -1,5 +1,6 @@
 # Permissions for fargate
 resource "aws_iam_role" "eks_fargate_role" {
+  count                 = var.use_fargate ? 1 : 0
   name                  = "${var.eks_cluster_name}-fargate_cluster_role"
   description           = "Allow fargate cluster to allocate resources for running pods"
   force_detach_policies = true
@@ -26,16 +27,19 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  count      = var.use_fargate ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.eks_fargate_role.name
+  role       = aws_iam_role.eks_fargate_role[0].name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  count      = var.use_fargate ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_fargate_role.name
+  role       = aws_iam_role.eks_fargate_role[0].name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  count      = var.use_fargate ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_fargate_role.name
+  role       = aws_iam_role.eks_fargate_role[0].name
 }
--- eks_node_group.tf
+++ eks_node_group.tf
@@ -1,8 +1,9 @@
 # Node group part
 resource "aws_eks_node_group" "eks_node_group" {
+  count           = var.use_fargate ? 0 : 1
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "${var.eks_cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  node_role_arn   = aws_iam_role.eks_node_group_role[0].arn
   subnet_ids      = aws_subnet.public_subnet.*.id
   capacity_type   = var.eks_node_group_instance_capacity_type
   disk_size       = var.eks_node_group_instance_disk_size
