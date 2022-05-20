1a53886124382e212cd04acaf8ab5271313b8455
Jeremy GOVI <jeremygovi@gmail.com>
2022-05-20 15:01:26 +0200

    hotfix: eks nodes must NOT pop on public subnet!

--- eks_node_group.tf
+++ eks_node_group.tf
@@ -4,7 +4,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "${var.eks_cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role[0].arn
-  subnet_ids      = aws_subnet.public_subnet.*.id
+  subnet_ids      = aws_subnet.private_subnet.*.id
   capacity_type   = var.eks_node_group_instance_capacity_type
 
   scaling_config {
