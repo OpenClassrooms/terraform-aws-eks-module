22ed42ebe2e16b14d66652eec8f3582a980f1cef
jeremygovi <jeremygovi@gmail.com>
2022-08-01 17:31:30 +0200

    add additional permissions for in case of codefresh EBS use

--- eks_node_group_role.tf
+++ eks_node_group_role.tf
@@ -270,7 +270,25 @@ resource "aws_iam_policy" "eks_node_group_custom_policy" {
                 "elasticloadbalancing:ModifyRule"
             ],
             "Resource": "*"
-        }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AttachVolume",
+                "ec2:CreateSnapshot",
+                "ec2:CreateTags",
+                "ec2:CreateVolume",
+                "ec2:DeleteSnapshot",
+                "ec2:DeleteTags",
+                "ec2:DeleteVolume",
+                "ec2:DescribeInstances",
+                "ec2:DescribeSnapshots",
+                "ec2:DescribeTags",
+                "ec2:DescribeVolumes",
+                "ec2:DetachVolume"
+            ],
+            "Resource": "*"
+    }
     ]
 }
 EOF
